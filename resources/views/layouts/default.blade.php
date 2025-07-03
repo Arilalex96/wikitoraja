0b0fd546352aaaf00a3adc945184b6b6d014998d
Arilalex96 <arilalex09@gmail.com>
2025-07-03 11:28:45 +0800

    update meta description

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
+        <meta name="description" content="WikiToraja - Platform berbagi pengetahuan tentang budaya dan warisan Toraja." />
         <meta name="author" content="" />
         <title>{{ ucwords($page_title) }} - {{ config('app.name') }}</title>
         <!-- Favicon-->
